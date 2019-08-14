a6137d6a2df8d50de1c3c02aff9a714c9eb3289f
Anton Jouline <juce66@gmail.com>
2019-08-14 11:29:34 -0700

    tweaks to matchset.lua

--- modules/matchset.lua
+++ modules/matchset.lua
@@ -2,17 +2,26 @@
 
 local m = {}
 
+-- difficulty: nil - game choice, 0 - beginner, 1 - amateur, ... 6 - Legend
+m.difficulty = 6
+
+-- extra time: nil - game choice, 0 - off, 1 - on
+m.extra_time = 0
+
+-- penalty shootout: nil - game choice, 0 - off, 1 - on
+m.penalties = 1
+
 function m.set_match_settings(ctx, options)
     if ctx.tournament_id == 65535 then
-        options.difficulty = 6 -- Legend difficulty
-        options.extra_time = 0 -- extra-time: off
-        options.penalties = 1  -- penalties: on
+        options.difficulty = m.difficulty
+        options.extra_time = m.extra_time
+        options.penalties = m.penalties
         return options
     end
 end
 
 function m.init(ctx)
-   ctx.register("set_match_settings", m.set_match_settings)
+    ctx.register("set_match_settings", m.set_match_settings)
 end
 
 return m
