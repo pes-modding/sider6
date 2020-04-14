8e2c82da01ceafa77a80fb64ff1f928341d2055e
Anton Jouline <juce66@gmail.com>
2020-04-14 16:03:22 -0700

    cleanup for env files

--- setenv.cmd
+++ setenv.cmd
@@ -1,7 +1,5 @@
 @echo off
 echo Setting kitserver compile environment
-@call "c:\vs11\vc\bin\x86_amd64\vcvarsx86_amd64.bat"
-rem set INCLUDE="soft\xinput1_3\include";%INCLUDE%
-rem set LIB="soft\xinput1_3\lib\x64";%LIB%
+@call "c:\program files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsx86_amd64.bat"
 echo Environment set
 
--- setenv2019.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Setting kitserver compile environment
-@call "c:\program files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsx86_amd64.bat"
-rem set INCLUDE="soft\xinput1_3\include";%INCLUDE%
-rem set LIB="soft\xinput1_3\lib\x64";%LIB%
-echo Environment set
-
